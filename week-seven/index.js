c401eb07aee19b3f3754f29b7d3242798116562f
Muzammil Bilwani <muzammil.bilwani@hotmail.com>
2023-03-26 19:36:10 +0500

    File updated

--- week-seven/index.js
+++ week-seven/index.js
@@ -1,3 +1,41 @@
-// My First JS Code
+console.log("Grocery Bill")
+console.log("-----------------")
+// Write a grocery bill for 3 items
 
-console.log("Hello World!")
+const ricePrice = 3.99
+const beansPrice = 2.99
+const eggsPrice = 1.99
+
+const riceQty = 2
+const beansQty = 3
+const eggsQty = 12
+
+const riceTotal = ricePrice * riceQty
+const beansTotal = beansPrice * beansQty
+const eggsTotal = eggsPrice * eggsQty
+
+console.log(`Rice: $${ricePrice} x ${riceQty}: $` + riceTotal)
+console.log(`Beans: $${beansPrice} x ${beansQty}: $` + beansTotal)
+console.log(`Eggs: $${eggsPrice} x ${eggsQty}: $` + eggsTotal)
+console.log("-----------------")
+
+const subtotal = riceTotal + beansTotal + eggsTotal
+let discount = 0
+if (subtotal > 20) {
+  console.log("You qualify for Discount that is 5% off")
+  discount = subtotal * 0.05
+}
+const tax = subtotal * 0.13
+
+const total = subtotal + tax - discount
+console.log("-----------------")
+
+console.log("Subtotal: $" + subtotal)
+console.log("Tax: $" + tax)
+if (discount > 0) {
+  console.log("Discount: $" + discount)
+}
+console.log("-----------------")
+
+console.log("Total: $" + total)
+console.log("-----------------")
