fbebb3b05a6fe5192fb7a4ab3355b86cc3d881b0
huzaifa <connectalhuzaifiii@gmail.com>
2023-06-20 23:41:53 +0500

    Update RecipeHome.jsx

--- week-react/reactapp/src/Recepie/RecipeHome.jsx
+++ week-react/reactapp/src/Recepie/RecipeHome.jsx
@@ -4,6 +4,7 @@ import { recipeList } from "./data"
 export const RecipeHome = () => {
   return (
     <div>
+      <h2>Just Testing Pull Resquest...</h2>
       <h1 className="text-4xl mt-8 mb-8">Recipe</h1>
       <div className="grid grid-rows-2 grid-cols-2 gap-12">
         {recipeList.map((recipe, index) => (
