cf0f8d161b2a045f901c1918788edfd2ab604734
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2023-06-21 18:35:56 +0500

    Assignment 8

--- week-react/reactapp/src/Recepie/RecipeHome.jsx
+++ week-react/reactapp/src/Recepie/RecipeHome.jsx
@@ -1,11 +1,16 @@
 import { RecipeItem } from "./RecipeItem"
 import { recipeList } from "./data"
+import { RecipeItem } from "./RecipeItem";
+import { recipeList } from "./data";
 
 export const RecipeHome = () => {
   return (
     <div>
       <h1 className="text-4xl mt-8 mb-8">Recipe</h1>
       <div className="grid grid-rows-2 grid-cols-2 gap-12">
+    <div className="m-6 p-6 content-center">
+      <h1 className="text-4xl mb-8">Recipe</h1>
+      <div className="grid grid-rows-2 grid-cols-2 gap-10 justify-items-center ">
         {recipeList.map((recipe, index) => (
           <RecipeItem key={index} recipe={recipe} />
         ))}
@@ -13,3 +18,5 @@ export const RecipeHome = () => {
     </div>
   )
 }
+  );
+};
