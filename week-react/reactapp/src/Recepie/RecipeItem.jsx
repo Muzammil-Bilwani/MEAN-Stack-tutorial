9c446976ae90fde31318e3f49570bd058b5eff38
mubashiralam89 <mubashiralam89@gmail.com>
2023-06-21 18:19:29 +0500

    modified the card width

--- week-react/reactapp/src/Recepie/RecipeItem.jsx
+++ week-react/reactapp/src/Recepie/RecipeItem.jsx
@@ -1,13 +1,13 @@
-import PropTypes from "prop-types"
-import { Link } from "react-router-dom"
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export const RecipeItem = ({ recipe }) => {
   return (
     <Link to={`/details/${recipe.id}`}>
-      <div className="shadow-lg rounded-2xl bg-white cursor-pointer hover:shadow-xl transition duration-200">
+      <div className="shadow-lg rounded-2xl w-fit bg-white cursor-pointer hover:shadow-xl transition duration-200">
         <img
           style={{
-            objectFit: "contain"
+            objectFit: "contain",
           }}
           className="w-72"
           src={recipe.image}
@@ -16,13 +16,13 @@ export const RecipeItem = ({ recipe }) => {
         <h5 className="p-4 font-sans text-2xl">{recipe.name}</h5>
       </div>
     </Link>
-  )
-}
+  );
+};
 
 RecipeItem.propTypes = {
   recipe: PropTypes.shape({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired
-  })
-}
+    image: PropTypes.string.isRequired,
+  }),
+};
