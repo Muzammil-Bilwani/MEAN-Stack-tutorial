091f583d7aded0c1058950d256fa45823adfcce9
mubashiralam89 <mubashiralam89@gmail.com>
2023-06-21 18:18:30 +0500

    updated the RecipeDetails component

--- week-react/reactapp/src/Recepie/RecipeDetails.jsx
+++ week-react/reactapp/src/Recepie/RecipeDetails.jsx
@@ -1,19 +1,27 @@
-import { useParams, useNavigate } from "react-router-dom"
-import { recipeList } from "./data"
+import { useParams, useNavigate } from "react-router-dom";
+import { recipeList } from "./data";
+import { RecipeItem } from "./RecipeItem";
+import { useEffect, useState } from "react";
 
 export const RecipeDetails = () => {
-  const { id } = useParams()
-  const navigate = useNavigate()
-
-  const recipe = recipeList[id - 1]
-
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const recipe = recipeList[id - 1];
+  const [otherRecipes, setOtherRecipes] = useState([]);
+  useEffect(() => {
+    setOtherRecipes(
+      recipeList.filter((e, i) => {
+        return e.id != id;
+      })
+    );
+  }, [id]);
   return (
     <div>
       <div className="flex justify-between items-center">
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={() => {
-            navigate(-1)
+            navigate(-1);
           }}
         >
           Back
@@ -24,7 +32,7 @@ export const RecipeDetails = () => {
         <div>
           <img
             style={{
-              objectFit: "contain"
+              objectFit: "contain",
             }}
             src={recipe.image}
             alt={recipe.name}
@@ -46,7 +54,7 @@ export const RecipeDetails = () => {
           <h1 className="text-4xl mt-8 mb-8">Instructions</h1>
           <ul
             style={{
-              textAlign: "justify"
+              textAlign: "justify",
             }}
             className="list-none list-inside"
           >
@@ -56,6 +64,18 @@ export const RecipeDetails = () => {
           </ul>
         </div>
       </div>
+      <hr className="my-10" />
+      <div>
+        <h1 className="text-4xl mb-8">Other recipes</h1>
+
+        <div className="flex justify-around gap-10 flex-wrap">
+          {otherRecipes.length > 0
+            ? otherRecipes.map((recipe, index) => (
+                <RecipeItem key={index} recipe={recipe} />
+              ))
+            : ""}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
